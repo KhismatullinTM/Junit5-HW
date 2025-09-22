c3297bd0aa2bcfcda4173dc3353385f9138a3869
Antonov Maksim <maks.antonov.2012@gmail.com>
2025-09-22 15:55:31 +0300

    first HW done

---
+++ src/test/java/com/example/demoqa/PageUrl.java
@@ -0,0 +1,14 @@
+package com.example.demoqa;
+
+public class PageUrl {
+    private String endPoint;
+
+    public String getEndPoint() {
+        return endPoint;
+    }
+
+    public PageUrl setEndPoint(String endPoint) {
+        this.endPoint = endPoint;
+        return this;
+    }
+}
